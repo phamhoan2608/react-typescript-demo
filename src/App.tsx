4a7c8b7b6c0994a3258834dc7f70b4cec70f469b
Phạm Văn Hoàn <60645075+phamhoan2608@users.noreply.github.com>
2022-10-25 07:43:39 +0700

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import BasicSelect from './components/BaseSelect';
+import SelectBox from './components/SelectBox';
+import { Route, Routes } from 'react-router-dom';
 
 // Tuple: fix elements, [string, number] => ["Pham Van Hoan", 20]
 // Enum: 
@@ -24,7 +27,7 @@ const reviews: {name: string; age: number; contact: [number, string]; permission
 {
   name: "Nguyễn Hà Anh",
   age: 22,
-  contact: [094567890, 'nguyenhaanh091100@gmail.com'],
+  contact: [194567890, 'nguyenhaanh091100@gmail.com'],
   permission: Permission.MODERATOR
 }
 ]
@@ -39,33 +42,41 @@ const user : { firstName: string; lastName: string; age: number; isStudent: bool
 }
 
 function App() {
+  
   const [count, setCount] = useState(0)
   function viewInfo(name: string, age?: number) {
     return  <h1>{name} + {age}</h1>
   }  
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      {viewInfo(reviews[0].name, reviews[0].age)}
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={ <SelectBox />}/>
+      <Route path="/home" element={ <h1> Pham Van Hoan </h1>}/>
+    </Routes>
+      {/* <BasicSelect/> */}
+    </>
+    // <div className="App">
+    //   <div>
+    //     <a href="https://vitejs.dev" target="_blank">
+    //       <img src="/vite.svg" className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://reactjs.org" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   {viewInfo(reviews[0].name, reviews[0].age)}
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </div>
   )
 }
 
